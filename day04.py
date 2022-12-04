b9edfe15ee8821a79e26dec51db25dae07e7f32a
Gabriel Reis <gabrielcnr@gmail.com>
2022-12-04 13:03:23 +0000

    part 2

--- day04.py
+++ day04.py
@@ -17,11 +17,36 @@ def part1(lines):
     return count
 
 
+def part2(lines):
+    count = 0
+    for line in lines:
+        range_1, range_2 = line.split(',')
+
+        a1, a2 = range_1.split('-')
+        b1, b2 = range_2.split('-')
+
+        a1, a2, b1, b2 = [int(n) for n in [a1, a2, b1, b2]]
+
+        does_not_overlap = ((a1 > b2) or (b1 > a2))
+        if not does_not_overlap:
+            # if ((a1 <= b2) and (b1 <= a2)):
+            print(f'{line = }')
+            count += 1
+    return count
+
+
 def test_part1():
     assert 2 == part1(read_input(test=True))
+
+
+def test_part2():
+    assert 4 == part2(read_input(test=True))
 
 
 if __name__ == '__main__':
     lines = read_input()
     p1 = part1(lines)
     print(f'Part 1: {p1}')
+
+    p2 = part2(lines)
+    print(f'Part 2: {p2}')
