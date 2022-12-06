ed3b52f175e6ac94db9be8931441cd4e64017835
Gabriel Reis <gabrielcnr@gmail.com>
2022-12-06 11:16:49 +0000

    part2

--- day05.py
+++ day05.py
@@ -4,7 +4,7 @@ import re
 from aoc import read_input
 
 
-def part1(lines):
+def part1(lines, make_move):
     stacks = defaultdict(list)
     for line in lines:
         if '[' in line:
@@ -19,9 +19,7 @@ def part1(lines):
             make_move(stacks, count, source, dest)
 
     print_stacks(stacks)
-    print(sum(len(v) for v in stacks.values()))
     return stacks
-    
 
 
 def print_stacks(stacks):
@@ -35,42 +33,53 @@ def print_stacks(stacks):
 #    stacks[source][:count] = []
 
 
-def make_move(stacks, count, source, dest):
-    if source == 4 or dest == 4:
-        print(f'move {count} from {source} to {dest}')
-        print('before', len(stacks[4]))
+def make_move1(stacks, count, source, dest):
     for _ in range(count):
         stacks[dest].insert(0, stacks[source].pop(0))
-    if source == 4 or dest == 4:        
-        print('after', len(stacks[4]))
-        print()
+
+
+def make_move2(stacks, count, source, dest):
+    stacks[dest] = stacks[source][:count] + stacks[dest]
+    stacks[source][:count] = []
 
 
 def test_make_move():
     stacks = {0: ['a', 'b', 'c'], 1: ['d', 'e']}
-    make_move(stacks, 1, 1, 0)
+    make_move1(stacks, 1, 1, 0)
     assert stacks == {0: ['d', 'a', 'b', 'c'], 1: ['e']}
 
-    make_move(stacks, 1, 0, 1)
+    make_move1(stacks, 1, 0, 1)
     assert stacks == {0: ['a', 'b', 'c'], 1: ['d', 'e']}
 
-    make_move(stacks, 2, 0, 1)
+    make_move1(stacks, 2, 0, 1)
     assert stacks == {0: ['c'], 1: ['b', 'a', 'd', 'e']}
 
-    make_move(stacks, 2, 1, 0)
+    make_move1(stacks, 2, 1, 0)
     assert stacks == {0: ['a', 'b', 'c'], 1: ['d', 'e']}
 
 
 def test_part1():
     lines = read_input(test=True)
-    stacks = part1(lines)
+    stacks = part1(lines, make_move1)
     assert [['C'], ['M'], ['Z', 'N', 'D', 'P']] == [v for k, v in sorted(stacks.items())]
     assert 'CMZ' == ''.join((v[0] for k, v in sorted(stacks.items())))
+
+
+def test_part2():
+    lines = read_input(test=True)
+    stacks = part1(lines, make_move2)
+    assert [['M'], ['C'], ['D', 'N', 'Z', 'P']] == [v for k, v in sorted(stacks.items())]
+    assert 'MCD' == ''.join((v[0] for k, v in sorted(stacks.items())))
+
 
 
 if __name__ == '__main__':
     lines = read_input()
 
-    p1 = part1(lines)
+    p1 = part1(lines, make_move1)
     p1_text = ''.join((v[0] for k, v in sorted(p1.items())))
     print(f'Part 1: {p1_text}')
+
+    p2 = part1(lines, make_move2)
+    p2_text = ''.join((v[0] for k, v in sorted(p2.items())))
+    print(f'Part 2: {p2_text}')
